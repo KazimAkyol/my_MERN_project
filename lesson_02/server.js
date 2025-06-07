765d8b1e2326aa812d35b07b40d7a507f1654793
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-07 23:11:01 +0200

    add lesson_02

---
+++ lesson_02/server.js
@@ -0,0 +1,27 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const { logger } = require('./middleware/logger')
+const PORT = process.env.PORT || 3500
+
+app.use(express.json())
+
+// Middleware'ler
+app.use(express.static(path.join(__dirname, 'public')))
+
+// Routes
+app.use('/', require('./routes/root'))
+
+// 404 Handler - DEĞİŞTİRİLMİŞ KISIM
+app.use((req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
