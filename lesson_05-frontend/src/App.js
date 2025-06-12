375f68b7940b89d3eac1d7c3353676f4a7dc71af
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-06-12 16:19:43 +0200

    added dashboard

--- lesson_05-frontend/src/App.js
+++ lesson_05-frontend/src/App.js
@@ -1,14 +1,18 @@
 import { Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
+import Public from "./components/Public";
+import Login from "./components/Login";
+import DashLayout from "./components/DashLayout";
 
 function App() {
-  return (
-    <Routes>
-        <Route path="/" element={<Layout />}>
-            
-        </Route>
-    </Routes>
-  );
+    return (
+        <Routes>
+            <Route path="/" element={<Layout />}>
+                <Route index element={<Public />} />
+                <Route path="login" element={<Login />} />
+            </Route>
+        </Routes>
+    );
 }
 
 export default App;
