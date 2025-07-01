a5c471de07fd07026ed55ed1131e922143fbb2d0
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-01 20:22:11 +0200

    updated store.js

--- lesson_07-frontend/src/features/users/UsersList.js
+++ lesson_07-frontend/src/features/users/UsersList.js
@@ -9,7 +9,11 @@ const UsersList = () => {
         isSuccess,
         isError,
         error,
-    } = useGetUsersQuery()
+    } = useGetUsersQuery(undefined, {
+        pollingInterval: 60000, // Poll every minute
+        refetchOnFocus: true,
+        refetchOnMountOrArgChange: true
+    })
 
     let content
 
