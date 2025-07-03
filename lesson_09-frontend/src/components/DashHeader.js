dfd413f3f04535e6dff16b6220a0952424d8e8a3
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-03 23:47:36 +0200

    added logoutButton

--- lesson_09-frontend/src/components/DashHeader.js
+++ lesson_09-frontend/src/components/DashHeader.js
@@ -1,19 +1,59 @@
 import { Link } from "react-router-dom"
-import {FontAweSomeIcon} from "@fortawesome/react-fontawesome"
-import {faRightFromBracket} from "@fortawesome/free-solid-svg-icons"
-import {useNavigate, Link, useLocation} from "react-router-dom"
+import { FontAweSomeIcon } from "@fortawesome/react-fontawesome"
+import { faRightFromBracket } from "@fortawesome/free-solid-svg-icons"
+import { useNavigate, Link, useLocation } from "react-router-dom"
 
-import {useSendLogoutNutation} from '../features/auth/authApiSlice'
+import { useSendLogoutNutation } from '../features/auth/authApiSlice'
+import { useEffect } from "react"
+
+const DASH_REGEX = /^\/dash(\/)?$/
+const NOTES_REGEX = /^\/dash\/notes(\/)?$/
+const USERS_REGEX = /^\/dash\/users(\/)?$/
 
 const DashHeader = () => {
+
+    const navigate = useNavigate()
+    const { pathname } = useLocation()
+
+    const [sendLogout, {
+        isLoading,
+        isSuccess,
+        isError,
+        error
+    }] = useSendLogoutNutation()
+
+    useEffect(() => {
+        if (isSuccess) navigate('/')
+    }, [isSuccess, navigate])
+
+    if (isLoading) return <p>Logging Out...</p>
+
+    if (isError) return <p>Error:{error.data?.message}</p>
+
+    let dashClass = null
+    if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname && !USERS_REGEX.test(pathname))) {
+        dashClass = "dash-header__container--small"
+    }
+
+    const logoutButton = (
+        <button
+            className="icon-button"
+            title="logout"
+            onClick={sendLogout}
+        >
+            <FontAweSomeIcon icon={faRightFromBracket} />
+        </button>
+    )
+
     const content = (
         <header className="dash-header">
-            <div className="dash-header__container">
+            <div className={`dash-header__container ${dashClass}`}>
                 <Link to="/dash">
                     <h1 className="dash-header__title">AkyolKDB techNotes</h1>
                 </Link>
                 <nav className="dash-header__nav">
                     {/* add nav buttons later */}
+                    {logoutButton}
                 </nav>
             </div>
         </header>
