52e223a34c827533174ba76e18b0b9adae2ccfc5
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-04 23:33:39 +0200

    added Bearer

--- lesson_10-frontend/src/app/api/apiSlice.js
+++ lesson_10-frontend/src/app/api/apiSlice.js
@@ -13,6 +13,30 @@ const baseQuery = fetchBaseQuery({
     }
 })
 
+const baseQueryWithReauth = async (args, api, extraOptions) => {
+    // console.log(args) // request url, method, body
+    // console.log(api) // signal, dispatch, getState()
+    // console.log(extraOptions) //custom like {shout: true}
+
+    let result = await baseQuery(args, api, extraOptions)
+
+    // if you wamt, handle other status codes, too
+    if (result?.error?.status === 403) {
+        console.log('sending refresh token')
+
+        // send refresh token to get new access token
+        const refreshResult = await baseQuery('/auth/refresh', api, extraOptions)
+
+        if (refreshResult?.data) {
+            
+            // store the new token
+            api.dispatch(setCredentials({...refreshResult.data}))
+
+            //
+        }
+    }
+}
+
 export const apiSlice = createApi({
     baseQuery,
     tagTypes: ['Note', 'User'],
