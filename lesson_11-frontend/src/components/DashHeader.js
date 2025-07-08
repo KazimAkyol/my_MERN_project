402375c8b611136840cb7f20f72eecf11b439e30
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-07-08 20:32:46 +0200

    added useAuth structure

--- lesson_11-frontend/src/components/DashHeader.js
+++ lesson_11-frontend/src/components/DashHeader.js
@@ -40,10 +40,6 @@ const DashHeader = () => {
     const onNotesClicked = () => navigate('/dash/notes')
     const onUsersClicked = () => navigate('/dash/users')
 
-    if (isLoading) return <p>Logging Out...</p>
-
-    if (isError) return <p>Error:{error.data?.message}</p>
-
     let dashClass = null
     if (!DASH_REGEX.test(pathname) && !NOTES_REGEX.test(pathname && !USERS_REGEX.test(pathname))) {
         dashClass = "dash-header__container--small"
@@ -137,7 +133,7 @@ const DashHeader = () => {
             <header className="dash-header">
                 <div className={`dash-header__container ${dashClass}`}>
                     <Link to="/dash">
-                        <h1 className="dash-header__title">techNotes</h1>
+                        <h1 className="dash-header__title">AkyolKDB techNotes</h1>
                     </Link>
                     <nav className="dash-header__nav">
                         {buttonContent}
