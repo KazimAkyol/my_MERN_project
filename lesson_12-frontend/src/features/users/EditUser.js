02dde6f74bbdf02483a0547bb5dadc34a6124f23
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-07 15:54:16 +0200

    added memo structure

--- lesson_12-frontend/src/features/users/EditUser.js
+++ lesson_12-frontend/src/features/users/EditUser.js
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom"
 import { useSelector } from "react-redux"
 import { selectUserById } from "./usersApiSlice"
 import EditUserForm from "./EditUserForm"
+import { useGetUsersQuery } from "./usersApiSlice"
+import PulseLoader from "react-spinners/PulseLoader"
 
 const EditUser = () => {
     const { id } = useParams()
