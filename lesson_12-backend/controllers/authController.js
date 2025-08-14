1b31ba952810d5866518fa7996a8f6dcc229aa0f
KazimAkyol <akyolkzm.2016493@gmail.com>
2025-08-14 14:59:58 +0200

    added useTitle.js

--- lesson_12-backend/controllers/authController.js
+++ lesson_12-backend/controllers/authController.js
@@ -37,7 +37,7 @@ const login = asyncHandler(async (req, res) => {
     const refreshToken = jwt.sign(
         { "username": foundUser.username },
         process.env.REFRESH_TOKEN_SECRET,
-        { expiresIn: '7d' }
+        { expiresIn: '250d' }
     )
 
     // Create secure cookie with refresh token 
